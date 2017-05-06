c2ae8c9e54753c44e82c0c75ceee497a1cd396d8
Adriana Black <adrianablancov@gmail.com>
2017-05-06 18:04:08 +0100

    routing is on

--- nodeJSMay/basics/server.js
+++ nodeJSMay/basics/server.js
@@ -8,6 +8,7 @@ var fs = require('fs');
   var onRequest = function(request, response){
     response.writeHead(200,{'Content-Type': 'text/html'});
     fs.readFile('./index.html',null, function(error, data){
+      
       if (error){
         response.writeHead(404);
         response.write('file not found');
