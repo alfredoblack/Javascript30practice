40bf52f937c5796cf5623dc058d6798d775ce3ef
Adriana Black <adrianablancov@gmail.com>
2017-05-07 18:40:36 +0100

    controller connected to html working

---
+++ nodeJSMay/contact-list-app/public/controller/controller.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http',
+
+function($scope, $http){
+  console.log('hello world from the control room');
+}
+
+
+  ])
