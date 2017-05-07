23c50871a1015dcb1efef9246a422a1278699007
Adriana Black <adrianablancov@gmail.com>
2017-05-07 17:38:12 +0100

    routes installed in one page

--- nodeJSMay/alf-express/routes/index.js
+++ nodeJSMay/alf-express/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
+
+router.get('/places', function(req, res, next) {
+  res.send('there are places i remember');
+});
+
+
 module.exports = router;
