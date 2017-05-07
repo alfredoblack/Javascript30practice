ddf452e2b4ab1275e5d9e708cdf30925a11f1479
Adriana Black <adrianablancov@gmail.com>
2017-05-07 20:31:12 +0100

    pulled data from controller to views

--- nodeJSMay/contact-list-app/public/controller/controller.js
+++ nodeJSMay/contact-list-app/public/controller/controller.js
@@ -3,7 +3,30 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 
 function($scope, $http){
   console.log('hello world from the control room');
-}
+  
+
+    personOne = {
+      name: 'adriana',
+      email: 'adriana@bob.com',
+      number: '111111111'
+    };
+
+    personTwo = {
+      name: 'Snoppy',
+      email: 'snoopy@bob.com',
+      number: '5555111111'
+    };
+
+    personThree = {
+      name: 'kelly',
+      email: 'kelly@bob.com',
+      number: '77777777'
+    };
 
 
-  ])
+
+var contactList= [personOne, personTwo, personThree];
+
+$scope.contactList = contactList;
+
+}]);
