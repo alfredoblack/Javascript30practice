2cbd9a071f391a591beb8bc96538ddf86434267a
Adriana Black <adrianablancov@gmail.com>
2017-05-08 22:54:48 +0100

    create and clear from client side to db

--- nodeJSMay/contact-list-app/public/controller/controller.js
+++ nodeJSMay/contact-list-app/public/controller/controller.js
@@ -1,37 +1,42 @@
-// var myApp = angular.module('myApp', []);
-// myApp.controller('AppCtrl',function($scope, $http){
-//   console.log('hello world from the control room');
-
-// $http.get('/contactList').then(successCallBack, errorCallBack);
-
-// function succesCallBack(response){
-//   console.log('I got the data I requested');
-//   $scope.contactList = response
-// }
-
-
 
 var myApp = angular.module('myApp', []);
+
 myApp.controller('AppCtrl', ['$scope', '$http',
 
 function($scope, $http){
   console.log('hello world from the control room');
 
-$http.get('/contactList').then(function(response){
-  console.log('I got the data I requested');
 
-  $scope.contactList = response.data;
-  console.log(response.data);
-});
+var refresh = function(){
+  $http.get('/contactList').then(function(response){
+    console.log('I got the data I requested');
 
-// $http.get('/contactList').then(function())
-
-// function succesCallBack(response){
-//   console.log('I got the data I requested');
-//   $scope.contactList = response
-// }
+    $scope.contactList = response.data;
+      console.log(response.data);
+    $scope.contact = null;
 
 
+  });
+
+}
+
+refresh();
+
+
+
+  $scope.addContact = function(){
+   console.log($scope.contact);
+   $http.post('/contactList', $scope.contact).then(function(response){
+    console.log(response);
+    
+   });
+
+   refresh();
+ }
+
+ // var clearInput = function(){
+ //  $scope.contact = null
+ // }
 
 
 }]);
--- nodeJSMay/contact-list-app/server.js
+++ nodeJSMay/contact-list-app/server.js
@@ -2,25 +2,32 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactList', ['contactList']);
+var bodyParser = require('body-parser');
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/contactList', function(req, res){
   console.log('i received a GET request');
 
-db.contactList.find(function(err, docs){
+  db.contactList.find(function(err, docs){
   console.log(docs);
   res.json(docs);
-  
+
 })
-
-
 
 });
 // app.get('/', function(req, res){
 //   res.send('hello world from server.js');
 // });
+
+app.post('/contactList', function(req, res){
+  console.log(req.body);
+  db.contactList.insert(req.body, function(err, doc){
+    res.json(doc);
+  })
+})
 
 app.listen(3000);
 console.log('server is running on port 3000');
